d8f17d40244bbd1b17b1f01b4bb1e945e3b8379a
Artyom Ulanchik <artyom.u@outlook.com>
2025-04-12 01:24:36 +0100

    version bump & tidy up

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@/assets/styles/global.scss";
+import "@/assets/styles/index.scss";
 
 const rootElement = document.getElementById("root")!;
 
